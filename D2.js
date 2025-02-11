57869df9b75d20a204a70a5dffe8ba53344b606d
MicheleFamoso <michelefamoso@gmail.com>
2025-02-11 16:45:20 +0100

    .

--- D2.js
+++ D2.js
@@ -4,23 +4,49 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let n1 = 7
+let n2 = 10
+if (n1 > n2) {
+  console.log("n1 è il piu grande")
+} else {
+  console.log("n2 è il piu grande")
+}
 /* ESERCIZIO 2
   Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+let number = 10
+if (number === 5) {
+  console.log("equal")
+} else {
+  console.log("not egual")
+}
 /* ESERCIZIO 3
   Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: usa l'operatore modulo)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+let numb = 15
+if (numb % 5 === 0) {
+  console.log("divisibile per 5")
+} else {
+  console.log("non dividibile per 5 ")
+}
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let numb3 = 4
+let numb4 = 8
+if (numb3 === 8 || numb4 === 8) {
+  console.log(" numb4 uguale a 8")
+} else if (numb3 + numb4 === 8 || numb3 - numb4 === 8) {
+  console.log("addizzione")
+} else {
+  console.log("nessuna condizione soddisfatta")
+}
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -29,6 +55,11 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+//let totalShoppingCart = 30
+//let spedizione = 10
+//if (totalShoppingCart >= 50) {
+//console.log("totale con spedizione gratutita", totalShoppingCart)
+//} else console.log("totale piu spedizione", totalShoppingCart + spedizione)
 
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
@@ -36,12 +67,23 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let totalShoppingCart = 30
+let spedizione = 10
+let sconto = totalShoppingCart * 0.2
+let totalSconto = totalShoppingCart - sconto
+
+if (totalShoppingCart >= 50) {
+  console.log("totale con spedizione gratutita", totalSconto)
+} else console.log("totale piu spedizione", totalSconto + spedizione)
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
   Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
   Alla fine mostra il risultato in console.
 */
+let x = 1
+let y = 2
+let z = 3
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -50,12 +92,19 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let valore = 30
+console.log(typeof valore)
 
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let operatore = 17
+let risultato = operatore % 2
+if (risultato > 0) {
+  console.log("Numero dispari", risultato)
+} else console.log("Numero Pari", risultato)
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
@@ -76,9 +125,9 @@
 */
 
 const me = {
-  name: 'John',
-  lastName: 'Doe',
-  skills: ['javascript', 'html', 'css'],
+  name: "John",
+  lastName: "Doe",
+  skills: ["javascript", "html", "css"],
 }
 
 /* SCRIVI QUI LA TUA RISPOSTA */
